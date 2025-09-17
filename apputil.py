d99ceb57f52d71b91a9f5e46be4b5999d8109bcc
Christian Martinez <martchfr@iu.edu>
2025-09-16 23:49:16 -0400

    Update apputil.py
    Added Fibonacci function

--- apputil.py
+++ apputil.py
@@ -2,4 +2,28 @@ import seaborn as sns
 import pandas as pd
 
 
-# update/add code below ...
+def fibonacci(n):
+    # Function internal variables
+    seq = [0, 1] 
+    iterator = 2
+    a = 0
+    b = 1
+
+    def recursive(iterator, a, b):
+        # Exit function
+        if iterator == n + 1:
+            return
+        
+        # Get next value in list and append
+        x = seq[a] + seq[b]
+        seq.append(x)
+
+        # Increment on variables for next call
+        iterator+=1
+        a+=1
+        b+=1
+
+        recursive(iterator, a, b)
+
+    recursive(iterator, a, b)
+    return seq
